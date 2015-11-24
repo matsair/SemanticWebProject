7b906811e9909b141441fd8a7e051af24e4be88a
matsair <matsschade@gmail.com>
2015-11-24 18:01:20 +0100

    working query for starters

--- App/SemanticQuizApp/app/src/main/java/com/matsschade/semanticquizapp/models/QueryStrings.java
+++ App/SemanticQuizApp/app/src/main/java/com/matsschade/semanticquizapp/models/QueryStrings.java
@@ -11,25 +11,25 @@ public class QueryStrings {
 
         queries = new QueryString [3][5];
 
-        String abc = "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
-                "PREFIX dbp: <http://dbpedia.org/property/>\n" +
-                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+        String abc = "PREFIX  dbo:  <http://dbpedia.org/ontology/>\n" +
+                "PREFIX  rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX  dbp:  <http://dbpedia.org/property/>\n" +
+                "PREFIX  dct:  <http://purl.org/dc/terms/>\n" +
+                "PREFIX  dbc:  <http://dbpedia.org/resource/Category:>\n" +
                 "\n" +
-                "SELECT DISTINCT ?city_name MAX(?sun)\n" +
+                "SELECT DISTINCT  ?city_name ?sun\n" +
                 "WHERE\n" +
-                "{\n" +
-                "  ?city rdf:type yago:City108524735 .\n" +
-                "  ?city rdfs:label ?city_name .\n" +
-                "  ?city dbo:country ?country .\n" +
-                "\n" +
-                "  ?city dbo:populationTotal ?population_total .\n" +
-                "  ?city dbp:yearSun ?sun .\n" +
-                "\n" +
-                "  FILTER (?population_total > 1000000) .\n" +
-                "  FILTER (langMatches(lang(?city_name), \"EN\")) .\n" +
-                "}\n" +
-                "ORDER BY DESC(?sun)\n";
+                "  { ?city  dct:subject  dbc:Capitals_in_Europe ;\n" +
+                "           rdfs:label   ?city_name ;\n" +
+                "           dbp:yearSun  ?sun\n" +
+                "    OPTIONAL\n" +
+                "      { ?city  dbo:populationTotal  ?population_total }\n" +
+                "    OPTIONAL\n" +
+                "      { ?city  dbp:populationBlank  ?population_blank }\n" +
+                "    FILTER ( ( ?population_total > 2000000 ) || ( ?population_blank > 2000000 ) )\n" +
+                "    FILTER langMatches(lang(?city_name), \"EN\")\n" +
+                "  }\n" +
+                "LIMIT   10";
 
         queries[0][0] = new QueryString(abc,"city_name", "callret-2");
 
